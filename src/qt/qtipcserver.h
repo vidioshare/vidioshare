52cfbee7bc370be92b39234c12fa3a2227e45d46
vidioshare <support@vidioshare.com>
2014-10-18 11:07:06 -0500

    vidioshare
    vidioshare checkpoint 2

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Vidio-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "VidioURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
